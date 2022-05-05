334a2b4bd6aafd7d2f7f80d30578062e791a30cd
Robert Dorna <rdorna8@gmail.com>
2022-05-05 19:50:10 +0200

    refactor: remove index.js file generator

--- src/schema/index.js
+++ src/schema/index.js
@@ -1,49 +1,6 @@
+const fs = require('fs')
 const { gql } = require('apollo-server')
 const { merge } = require('lodash')
-const {
-  typeDefs: Meal,
-  resolvers: mealResolvers
-} = require('./basic/meal')
-const {
-  typeDefs: Mealtime,
-  resolvers: mealtimeResolvers
-} = require('./basic/mealtime')
-const {
-  typeDefs: Goal,
-  resolvers: goalResolvers
-} = require('./basic/goal')
-const {
-  typeDefs: Water,
-  resolvers: waterResolvers
-} = require('./basic/water')
-const {
-  typeDefs: Product,
-  resolvers: productResolvers
-} = require('./basic/product')
-const {
-  typeDefs: Buy,
-  resolvers: buyResolvers
-} = require('./basic/buy')
-const {
-  typeDefs: Eat,
-  resolvers: eatResolvers
-} = require('./basic/eat')
-const {
-  typeDefs: Items,
-  resolvers: itemsResolvers
-} = require('./screens/items')
-const {
-  typeDefs: Diet,
-  resolvers: dietResolvers
-} = require('./screens/diet')
-const {
-  typeDefs: Products,
-  resolvers: productsResolvers
-} = require('./screens/products')
-const {
-  typeDefs: User,
-  resolvers: userResolvers
-} = require('./screens/user')
 
 
 const Base = gql`
@@ -58,33 +15,22 @@ const Base = gql`
   }
 `;
 
-const typeDefs = [
-  Base,
-  Meal,
-  Mealtime,
-  Goal,
-  Water,
-  Product,
-  Buy,
-  Eat,
-  Items,
-  Diet,
-  Products,
-  User,
-]
+let typeDefs = [Base]
+let resolvers = {}
 
-const resolvers = merge({},
-  mealResolvers,
-  mealtimeResolvers,
-  goalResolvers,
-  waterResolvers,
-  productResolvers,
-  buyResolvers,
-  eatResolvers,
-  itemsResolvers,
-  dietResolvers,
-  productsResolvers,
-  userResolvers,
-)
+const require_dir = dir => {
+  fs.readdirSync(`src/schema/${dir}`).forEach(file => {
+    const {
+      typeDefs: Type,
+      resolvers: typeResolvers
+    } = require(`./${dir}/${file}`)
+
+    typeDefs.push(Type)
+    resolvers = merge(resolvers, typeResolvers)
+  })
+}
+
+require_dir("basic")
+require_dir("screens")
 
 module.exports = { typeDefs, resolvers }
